2bfecd994ec1af5ca51f8eaa3f3036a57d01b7e0
Artur de Maria <arturdemaria7@gmail.com>
2025-01-29 00:05:58 -0300

    update: autores page fixed

--- src/Components/Pages/Autores.jsx
+++ src/Components/Pages/Autores.jsx
@@ -5,58 +5,34 @@ function Autores() {
       className="bg-white p-10 border-1 border-slate-300 rounded-lg shadow-xl"
     >
       <h1 className="font-bold text-2xl mb-4">Autores</h1>
-      <div className="flex-column gap-5 flex">
-        <article>
-          <h2 className="font-bold text-green-700 text-lg">Nome do Autor</h2>
-          <div className="min-[950px]:flex gap-5">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDAW3c4VeojGKTpXQCtoLQG0_VWjzJwVdAEQ&s"
-              alt="Foto do Autor"
-              className="max-w-[8rem] max-h-[8rem] min-w-2rem min-h-[2rem]"
-            />
-            <div className="w-full flex flex-col justify-between">
-              <p className="mb-2 overflow:hidden">
-                Descrição sobre o trabalho do autor: Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit. Sunt neque non dignissimos, ab
-                voluptatum ipsa at placeat perspiciatis excepturi
-                exercitationem, magni libero ad autem ut illo recusandae omnis
-                architecto aut.
-              </p>
-              <button
-                type="button"
-                className="btn btn-outline-dark w-full mt-auto hover:bg-green-600 border-green-600"
-              >
-                Página do autor na Wikipedia
-              </button>
+      <div className="flex-column min-xl:flex-nowrap flex-wrap gap-5 flex">
+        {[...Array(2)].map((e, i) => (
+          <article key={i}>
+            <h2 className="font-bold text-green-700 text-lg">Nome do Autor</h2>
+            <div className="min-[950px]:flex gap-5">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDAW3c4VeojGKTpXQCtoLQG0_VWjzJwVdAEQ&s"
+                alt="Foto do Autor"
+                className="max-w-[8rem] max-h-[8rem] min-w-2rem min-h-[2rem]"
+              />
+              <div className="w-full flex flex-col justify-between">
+                <p className="mb-2 overflow:hidden">
+                  Descrição sobre o trabalho do autor: Lorem ipsum dolor sit
+                  amet consectetur, adipisicing elit. Sunt neque non
+                  dignissimos, ab voluptatum ipsa at placeat perspiciatis
+                  excepturi exercitationem, magni libero ad autem ut illo
+                  recusandae omnis architecto aut.
+                </p>
+                <button
+                  type="button"
+                  className="w-full mt-auto hover:bg-green-600 hover:text-white border-green-600 border rounded-md"
+                >
+                  Página do autor na Wikipedia
+                </button>
+              </div>
             </div>
-          </div>
-        </article>
-
-        <article>
-          <h2 className="font-bold text-green-700 text-lg">Nome do Autor</h2>
-          <div className="min-[950px]:flex gap-5">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDAW3c4VeojGKTpXQCtoLQG0_VWjzJwVdAEQ&s"
-              alt="Foto do Autor"
-              className="max-w-[8rem] max-h-[8rem] min-w-2rem min-h-[2rem]"
-            />
-            <div className="w-full flex flex-col justify-between">
-              <p className="mb-2 overflow:hidden">
-                Descrição sobre o trabalho do autor: Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit. Sunt neque non dignissimos, ab
-                voluptatum ipsa at placeat perspiciatis excepturi
-                exercitationem, magni libero ad autem ut illo recusandae omnis
-                architecto aut.
-              </p>
-              <button
-                type="button"
-                className="btn btn-outline-dark w-full mt-auto hover:bg-green-600 border-green-600"
-              >
-                Página do autor na Wikipedia
-              </button>
-            </div>
-          </div>
-        </article>
+          </article>
+        ))}
       </div>
     </section>
   );
